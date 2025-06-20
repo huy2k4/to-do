f849f37a6b9100f4c1ac3aa5a531fbc41029796d
huy2k4 <huyngokngeck@gmail.com>
2025-06-20 16:00:41 +0700

    update 0.21

--- start.bat
+++ start.bat
@@ -1,4 +1,11 @@
 @echo off
-echo  Đang khởi động TodoList App...
-start "" "http://localhost:3000"
-json-server --watch D:\TODOLIST\mock-api\db.json --host 0.0.0.0 --port 3001
+
+cd /d %~dp0
+
+docker-compose down
+docker-compose build
+docker-compose up -d
+
+start http://localhost:3000
+
+pause
